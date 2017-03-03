fddbbf16f2c3a2c370a57a8e7f4f777c08a2ef42
Nathan Cooper <nac33@students.uwf.edu>
2017-03-03 00:06:45 -0600

    Added Anthony's DoWhat implementation with unit tests

---
+++ DoWhatApplication/UnitTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Google.Apis.CloudSpeechAPI.v1beta1;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using System.IO;
+using Google.Apis.CloudSpeechAPI.v1beta1.Data;
+using DoWhatImplementation;
+
+namespace UnitTestDoWhatImplementation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreateAuthorizedClient()
+        {
+            //arrange
+
+            //act
+            CloudSpeechAPIService service = DoWhat.CreateAuthorizedClient();
+            //assert
+            Assert.IsNotNull(service);
+        }
+        [TestMethod]
+        public void TestSendToSpeech()
+        {
+            //arrange
+            String inputFile = @"..\..\..\resources\audio.raw";
+            DoWhat sut = new DoWhat();
+            sut.setAudioFileLocation(inputFile);
+            //act
+            sut.SendToSpeech();
+            String result = sut.getSTTString();
+            //assert
+            Assert.AreEqual(" how old is the Brooklyn Bridge", result, true);
+        }
+        [TestMethod]
+        public void TestInputFile()
+        {
+            //arrange
+            String inputFile = "";
+            inputFile = @"..\..\..\resources\audio.raw";
+            //act
+            //assert
+            Assert.IsNotNull(inputFile);
+        }
+        [TestMethod]
+
+        public void TestSetAudioFileLocation()
+        {
+            //arrange
+            String inputFile = @"..\..\..\resources\audio.raw";
+            DoWhat sut = new DoWhat();
+            //act
+            sut.setAudioFileLocation(inputFile);
+            String result = sut.getAudioFileLocation();
+            //assert
+            Assert.AreEqual(result, inputFile, true);
+        }
+        
+        
+    }
+}
