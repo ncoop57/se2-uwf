22b92dadd88f7bb6b138a80d39c0928c0805351d
drumboys1000 <jrp58@students.uwf.edu>
2017-03-06 19:31:03 -0600

    Added a new project to store the Android library references since I wasn't able to add them to the the other ones. I forgot how to go about it but I think the other projects would need to be added as references, so that the MainActivity can use the implementations. Also two new classes:
    + Audio
    	• Right now it records and encodes in AMR-WB which is supported by the Speech API but isn't lossless.
    	• I WON'T be able to get it to encod and savee in FLAC as Android doesn't support that format BUT I can try to get in in PCM 16, which is lossless and supported by the Speech API and Android
    
    + MainActivity - think of it like a main method for the app itself. Right now it's not complete and I've only used ot so far to check and see if basic UI and recording works. (They do.)

---
+++ DoWhatApplication/DoWhatapp/Audio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using Android.Media;
+// Set up RECORD_AUDIO permission in the AndroidManifest.xml before use.
+namespace DoWhatapp
+{
+	public class Audio
+	{
+		protected MediaRecorder c_recorder;		// compressed audio recorder
+
+		/* Try this if the RecordAudioLossless method doesn't work */
+		public void RecordAudioCompressed(string filePath) 
+		{
+			try
+			{
+				if (File.Exists(filePath))
+				{
+					File.Delete(filePath);
+				}
+				if (c_recorder == null)
+				{
+					c_recorder = new MediaRecorder();     // Initial state
+				}
+				else
+				{
+					c_recorder.Reset();
+				}
+				c_recorder.SetAudioSource(AudioSource.Mic);
+				c_recorder.SetOutputFormat(OutputFormat.AmrWb);
+				c_recorder.SetAudioEncoder(AudioEncoder.AmrWb);
+				// Initialized state.
+				c_recorder.SetOutputFile(filePath);
+				// DataSourceConfigured state.
+				c_recorder.Prepare();     // Prepared state.
+				c_recorder.Start();		// Recording state.
+			}
+			catch (Exception e)
+			{
+				Console.Out.WriteLine(e.StackTrace);
+			}
+		}
+
+		/* Lossless audio recorder */
+		public void RecordAudioLossless()
+		{
+			
+		}
+
+		public void Stop()
+		{
+			StopRecorder();
+		}
+
+		public void StopRecorder()
+		{
+			if (c_recorder != null)
+			{
+				c_recorder.Stop();
+				c_recorder.Release();
+				c_recorder = null;
+			}
+		}
+
+
+
+	}
+}
---
+++ DoWhatApplication/DoWhatapp/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿/* TESTED TO WORK - audio is being stored properly
+*
+* To find the file after recording, go to Settings on your device. Go to Internal Storage and then Explore. 
+* Pathname: /Android/data/com.companyname.dowhatapp/files/audiotest.awb
+*
+*/
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Media;
+// This class allows for the app to run. This will be cleaned up as I do more Android stuff
+namespace DoWhatapp
+{
+	[Activity(Label = "DoWhatapp", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		Audio audio = new Audio();
+		Button record = null;
+		bool isRecording = false;
+		String fileName = null;
+
+		private void onRecord(bool start)
+		{
+			if (start)
+			{
+				audio.RecordAudioCompressed(fileName);
+			}
+			else
+			{
+				audio.Stop();
+			}
+		}
+
+		/* runs the app */
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Record to our directory
+			fileName = GetExternalFilesDir(null).AbsolutePath;
+			fileName += "/audiotest.awb";
+
+
+			//Record button
+			record = FindViewById<Button>(Resource.Id.record);
+
+			record.Enabled = true;
+			record.Click += (object sender, EventArgs e) =>
+			{
+				isRecording = !isRecording;
+				onRecord(isRecording);
+			};
+
+		}
+
+	}
+}
+
