eacdfcdbfa223e0c8c3ae64e37379965a31a435c
agl11 <agl11@students.uwf.edu>
2017-03-21 23:33:15 -0500

    adding authorship comments and cleaned up extraneous methods

--- DoWhatApplication/DoWhatImplementation/DoWhat.cs
+++ DoWhatApplication/DoWhatImplementation/DoWhat.cs
@@ -11,14 +11,12 @@ namespace DoWhatImplementation
 {
     public class DoWhat
     {
-        //instance variables coded agl11
+        //instance variables coded agl11 and nac33
         private String audioFileLocation;   //(.flac)
-        //private Stream audioStream; //.flac     //we will use whichever Julien can coax out of the UI
         private String STTString;
         private String verb;
         private String subject;
         private String target;
-
         private List<string> commands = "open play search".Split(' ').ToList();
         private List<string> skipWords;
         
@@ -32,15 +30,6 @@ namespace DoWhatImplementation
         {
             this.audioFileLocation = inputAudioFileLocation;
         }
-        //code in place in case we decide to use a stream instead
-        /*public Stream getAudioStream()
-        {
-            return this.audioStream;
-        }
-        public void setAudioStream(Stream inputStream)
-        {
-            this.audioStream = inputStream;
-        }*/
         public String getSTTString()
         {
             return this.STTString;
@@ -73,7 +62,7 @@ namespace DoWhatImplementation
         {
             this.target = inputString;
         }
-        //default constructor builds with null coded agl11
+        //default constructor builds with null coded agl11 and nac33
         public DoWhat()
         {
             //audioFile = file from record;
@@ -81,38 +70,30 @@ namespace DoWhatImplementation
             this.verb = "";
             this.subject = "";
             this.target = "";
-
             string line;
             skipWords = new List<string>();
-
-            // Read the file and display it line by line.
+            // Read the file and display it line by line coded nac33
             System.IO.StreamReader file =
                new System.IO.StreamReader("../../../stopwords.txt");
             while ((line = file.ReadLine()) != null)
             {
                 skipWords.Add(line);
             }
-
             file.Close();
-
         }
         //coded agl11
         public void SendToSpeech()
         {
-
             IList<string> list = new List<string>();
             list.Add("Wink Hub");
             list.Add("Todist");
-
             SpeechContext context = new SpeechContext();
             context.Phrases = list;
-
             String inputFileString = this.getAudioFileLocation();
             String outputProcessedString = "";
-
-            //authentication
+            //authentication coded agl11
             CloudSpeechAPIService service = CreateAuthorizedClient();
-            //construction of request
+            //construction of request coded agl11
             SyncRecognizeRequest request = new Google.Apis.CloudSpeechAPI.v1beta1.Data.SyncRecognizeRequest()
             {
                 Config = new Google.Apis.CloudSpeechAPI.v1beta1.Data.RecognitionConfig()
@@ -155,55 +136,37 @@ namespace DoWhatImplementation
                 ApplicationName = "DoWhat Application Audio",
             });
         }
-
+        //NLP coded nac33
         public void ProcessViaNLP(string input)
         {
-
             input = input.ToLower();
-
             foreach (string word in commands)
             {
-
                 if (input.Contains(word))
                 {
-
                     input = string.Join(" ", input.Split(' ').Except(commands).Except(skipWords));
-
                     this.setVerb(word);
                     this.setSubject(input);
-
                     this.execute(word, input);
-
                 }
-
             }
-
         }
-
+        //coded nac33
         public void execute(string command, string input)
         {
-
             if (command.Equals("open"))
             {
-
                 Console.WriteLine("Opening " + input);
-
             }
             else if (command.Equals("play"))
             {
-
                 Console.WriteLine("Playing " + input);
-
             }
             else if (command.Equals("search"))
             {
-
                 Console.WriteLine("Searching " + input);
-
             }
-
         }
-
     }
 }
 
