f68b4ff93b2f23db53573789a4303d9984169dea
Nathan Cooper <nac33@students.uwf.edu>
2017-04-01 10:36:29 -0500

    Add comments to MainActivity file

--- DoWhat/DoWhat/MainActivity.cs
+++ DoWhat/DoWhat/MainActivity.cs
@@ -15,18 +15,22 @@ namespace DoWhat
     public class MainActivity : Activity
     {
 
+        // Instances variable
         private ISpeech speech;
         private IStringMatcher commandMatcher;
         private IStringMatcher applicationMatcher;
         private Button recordBtn;
         private TextView textBox;
 
+        // Main entry point for our application
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
+            // Create the speech object to use for speech recognition
             speech = new Speech(10);
-
+            
+            // Initialize the command matcher and application matcher to use to parse the user's input
             commandMatcher = new CommandStringMatcher(this.Assets.Open("dictionary.txt"));
             applicationMatcher = new ApplicationStringMatcher(this.getApplications());
 
@@ -35,11 +39,11 @@ namespace DoWhat
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // get the resources from the layoutW
+            // get the resources from the layout
             recordBtn = FindViewById<Button>(Resource.Id.recordBtn);
             textBox = FindViewById<TextView>(Resource.Id.outputTxt);
 
-
+            // Create a button click event for the recordBtn
             recordBtn.Click += delegate
             {
                 
@@ -52,6 +56,7 @@ namespace DoWhat
 
         }
 
+        // Some default method which is triggered when the result of the android speech returned
         protected override void OnActivityResult(int requestCode, Result resultVal, Intent data)
         {
 
@@ -70,11 +75,16 @@ namespace DoWhat
                         textBox.Text = textInput;
 
 
-
+                        // Process the user's input and parsing the command the user said
                         string processed = commandMatcher.process(textInput);
+
+                        // Process the user's input and parsing the application the user said
                         applicationMatcher.process(processed);                        
 
+                        // Create an action to handle what the user is requesting the app to do
                         Action action = new Action(commandMatcher, applicationMatcher, this);
+
+                        // Perform the action
                         action.run();
 
                     }
@@ -88,28 +98,46 @@ namespace DoWhat
             base.OnActivityResult(requestCode, resultVal, data);
         }
 
+        /**
+         * Launch the application on the pone with the given application package name
+         */
         public void openApplication(string application)
         {
 
+            // Set up the intent which will be used for launching the application
             Intent intent = PackageManager.GetLaunchIntentForPackage(application);
+
+            // Launch the application
             StartActivity(intent);
 
         }
 
+        /**
+         * Get all of the application package names that are currently stored on the phone as and store them as a list
+         * @return the list of application package names
+         */
         public IList<string> getApplications()
         {
 
+            // Create a list to store all of the applications
             IList<string> applications = new List<string>();
 
+            // Run through each of the applications installed on the phone
             foreach (var item in PackageManager.GetInstalledApplications(new Android.Content.PM.PackageInfoFlags()))
             {
 
+                // Get the context which has the name of the application package from each of the items
                 var context = CreatePackageContext(item.PackageName, PackageContextFlags.IgnoreSecurity);
+
+                // Add the name of the application package to the application list
                 applications.Add(context.PackageName);
+
+                // Print out the application package names to the console
                 Console.WriteLine(context.PackageName);
 
             }
 
+            // Return the list of application package names that are installed on the phone
             return applications;
 
         }
